e8559b36cd8b7e443ccf04112fdcc27263300cb1
LisaWesthaeusser <lisawesthaeusser@gmail.com>
2017-05-22 11:54:52 +0200

    Test

---
+++ TestJueliLisa/src/htwg/test/TestJueliLisaServlet.java
@@ -0,0 +1,12 @@
+package htwg.test;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class TestJueliLisaServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
