bf91ffe589c6a2f01e57911b0d762c0d5634ace6
LisaWesthaeusser <lisawesthaeusser@gmail.com>
2017-05-28 18:17:23 +0200

    Uuuuups, doch im Master gearbeitet...

--- TestJueliLisa/src/htwg/test/LisasServlet.java
+++ TestJueliLisa/src/htwg/test/LisasServlet.java
@@ -1,7 +1,62 @@
 package htwg.test;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class LisasServlet extends HttpServlet{
+import daodi.Patient;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@SuppressWarnings("serial")
+public class LisasServlet extends HttpServlet {
+
+	ResultSet result = null;
+	Statement st = null;
+	Connection con = null;
+	String kette = null;
+	Bogen bogen = null;
+
+	public Bogen readBogen(String id) {
+
+		try {
+			con = DriverManager.getConnection("jdbc:sqlite:Datenbank.db");
+			st = con.createStatement();
+			result = st.executeQuery("SELECT * FROM BoBogen WHERE BoId = " + id);
+
+			if (result.next()) {
+				bogen = SQLSelect(id, result);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return bogen;
+	}
+
+	private Bogen SQLSelect(String id, ResultSet result) throws SQLException {
+
+		bogen.setFrage1(result.getString("BoFrage1"));
+		bogen.setId(result.getInt("BoId"));
+		return bogen;
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		
+		String bogenId = req.getParameter("ID");
+		Bogen bogen = readBogen(bogenId);
+
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		String htmlResponse = "<html>";
+		htmlResponse += "<head>";
+		htmlResponse += "<body>";
+	    htmlResponse +=  "<h2>Ihr Benutzername lautet: " + bogen.getFrage1() + "</h2>";
+		htmlResponse += "</body>";
+		htmlResponse += "</html>";		
+		writer.println(htmlResponse);
+	}
 }
---
+++ TestJueliLisa/src/htwg/test/SQLiteJDBC.java
@@ -0,0 +1,17 @@
+package htwg.test;
+
+import java.sql.*;
+
+public class SQLiteJDBC {
+	public static void main(String args[]) {
+		Connection c = null;
+		try {
+			Class.forName("");
+			c = DriverManager.getConnection("PatientDB.db");
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Opened database successfully");
+	}
+}
